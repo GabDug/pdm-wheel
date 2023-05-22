2c8254627eecf0e13605d4d39954ba4e1358116f
Gabriel Dugny <git@dugny.me>
2023-05-23 01:25:26 +0200

    chore: fix for newer PDM versions

--- src/pdm_wheel/wheel.py
+++ src/pdm_wheel/wheel.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 import os
 import shutil
 from pathlib import Path
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Iterable
 
-from pdm.cli.actions import resolve_candidates_from_lockfile
+from pdm.cli.actions import check_lockfile, resolve_candidates_from_lockfile
 from pdm.cli.commands.base import BaseCommand
+from pdm.cli.filters import GroupSelection
 from pdm.cli.options import groups_group, lockfile_option
-from pdm.cli.utils import check_project_file, translate_groups
+from pdm.cli.utils import check_project_file
+from pdm.models.candidates import Candidate
 
 if TYPE_CHECKING:
     import argparse
@@ -19,7 +21,7 @@ if TYPE_CHECKING:
 
 class WheelCommand(BaseCommand):
     """PDM implementation of `pip wheel`
-    Build Wheel archives for your requirements and dependencies.
+    Build Wheel archives for your requirements and dependencies, from your lockfile.
     """
 
     def add_arguments(self, parser: argparse.ArgumentParser) -> None:
@@ -30,29 +32,30 @@ class WheelCommand(BaseCommand):
             "--wheel-dir",
             dest="wheel_dir",
             metavar="dir",
-            default=os.curdir,
-            help="Specify the directory to save wheels, where the default is the current directory",
+            default=None,
+            help="Specify the directory to save wheels, where the default is ./wheels",
         )
 
     def handle(self, project: Project, options: argparse.Namespace) -> None:
+        # XXX --force flag to ignore warnings?
         check_project_file(project)
+        check_lockfile(project, raise_not_exist=True)
 
         project.environment.get_working_set()
         groups: list[str] = list(options.groups)
 
-        groups = translate_groups(
-            project,
-            options.default,
-            options.dev,
-            options.groups or (),
-        )
+        selection = GroupSelection(project, groups=groups, default=False)
         requirements: dict[str, Requirement] = {}
-        for group in groups:
-            requirements.update(project.get_dependencies(group))
 
+        for group in selection:
+            requirements.update(project.get_dependencies(group=group))
+        selection = GroupSelection.from_options(project, options)
+        requirements: dict[str, Requirement] = {}
+        for group in selection:
+            requirements.update(project.get_dependencies(group))
         project.core.ui.echo(
             "The exported wheels are no longer cross-platform. "
-            "Using it on other platforms may cause unexpected result.",
+            "Using it them other platforms may cause unexpected result.",
             style="warning",
             err=True,
         )
@@ -62,13 +65,19 @@ class WheelCommand(BaseCommand):
         (candidate for candidate in candidates.values() if not candidate.req.extras)
 
         # Create output directory if it doesn't exist
-        wheel_dir = Path(options.wheel_dir)
+        wheel_dir = (
+            Path(options.wheel_dir)
+            if options.wheel_dir
+            else Path(os.getcwd()).joinpath("wheels")
+        )
         wheel_dir.mkdir(parents=True, exist_ok=True)
 
         build_failures = []
-
         for candidate_name, candidate in candidates.items():
-            project.core.ui.echo(f"Building wheel for {candidate_name} {candidate.version}... ",  err=False)
+            project.core.ui.echo(
+                f"Building wheel for {candidate_name} {candidate.version}... ",
+                err=False,
+            )
 
             environment = project.environment
             prepared_candidate = candidate.prepare(environment=environment)
@@ -85,7 +94,7 @@ class WheelCommand(BaseCommand):
                 )
                 # build_failures.append(req)
             else:
-                project.core.ui.echo(f"Saved ./{rel_path}", err=False)
+                project.core.ui.echo(f"Saved {rel_path}", err=False)
 
         if len(build_failures) > 0:
             project.core.ui.echo(
