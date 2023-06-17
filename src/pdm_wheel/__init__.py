81b277d073119ae458047419f4f42def0eeae4e5
Gabriel Dugny <git@dugny.me>
2023-06-17 14:43:58 +0200

    chore: add pdm as dependency

--- src/pdm_wheel/__init__.py
+++ src/pdm_wheel/__init__.py
@@ -4,7 +4,6 @@ from typing import TYPE_CHECKING
 
 from pdm.signals import post_build
 
-from pdm_wheel.check_built_wheel import post_build_listener
 from pdm_wheel.wheel import ExportWheelsCommand
 
 if TYPE_CHECKING:
@@ -15,4 +14,4 @@ if TYPE_CHECKING:
 def wheel_plugin(core: Core) -> None:
     """Register wheel command and post_build hook with PDM."""
     core.register_command(ExportWheelsCommand, "wheel")
-    post_build.connect(post_build_listener)
+
